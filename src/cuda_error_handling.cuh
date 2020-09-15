9b9540a697e92d74b308cca8089be4fb6d61e773
J. Tian <tian.jn09@gmail.com>
2020-09-15 17:08:55 -0500

    add new error handling snippets for CUDA and CUSPARSE

--- src/cuda_error_handling.cuh
+++ src/cuda_error_handling.cuh
@@ -2,8 +2,11 @@
 #define CUDA_ERROR_HANDLING
 
 #include <cuda_runtime.h>
+#include <cusparse.h>
+
 #include <cstdio>
 
+// back compatibility start
 static void HandleError(cudaError_t err, const char* file, int line)
 {
     if (err != cudaSuccess) {
@@ -12,5 +15,37 @@ static void HandleError(cudaError_t err, const char* file, int line)
     }
 }
 #define HANDLE_ERROR(err) (HandleError(err, __FILE__, __LINE__))
+// back compatibility end
+
+static void check_cuda_error(cudaError_t status, const char* file, int line)
+{
+    if (cudaSuccess != status) {
+        printf("CUDA API failed at \e[31m\e[1m%s:%d\e[0m with error: %s (%d)\n", file, line, cudaGetErrorString(status), status);
+        exit(EXIT_FAILURE);
+    }
+}
+
+static void check_cusparse_error(cusparseStatus_t status, const char* file, int line)
+{
+    if (CUSPARSE_STATUS_SUCCESS != status) {
+        printf("\nCUSPARSE status reference:\n");
+        printf("CUSPARSE_STATUS_SUCCESS                   -> %d\n", CUSPARSE_STATUS_SUCCESS);
+        printf("CUSPARSE_STATUS_NOT_INITIALIZED           -> %d\n", CUSPARSE_STATUS_NOT_INITIALIZED);
+        printf("CUSPARSE_STATUS_ALLOC_FAILED              -> %d\n", CUSPARSE_STATUS_ALLOC_FAILED);
+        printf("CUSPARSE_STATUS_INVALID_VALUE             -> %d\n", CUSPARSE_STATUS_INVALID_VALUE);
+        printf("CUSPARSE_STATUS_ARCH_MISMATCH             -> %d\n", CUSPARSE_STATUS_ARCH_MISMATCH);
+        printf("CUSPARSE_STATUS_EXECUTION_FAILED          -> %d\n", CUSPARSE_STATUS_EXECUTION_FAILED);
+        printf("CUSPARSE_STATUS_INTERNAL_ERROR            -> %d\n", CUSPARSE_STATUS_INTERNAL_ERROR);
+        printf("CUSPARSE_STATUS_MATRIX_TYPE_NOT_SUPPORTED -> %d\n", CUSPARSE_STATUS_MATRIX_TYPE_NOT_SUPPORTED);
+        printf("CUSPARSE_STATUS_NOT_SUPPORTED             -> %d\n", CUSPARSE_STATUS_NOT_SUPPORTED);
+        printf("CUSPARSE_STATUS_INSUFFICIENT_RESOURCES    -> %d\n", CUSPARSE_STATUS_INSUFFICIENT_RESOURCES);
+        printf("\n");
+        printf("CUSPARSE API failed at \e[31m\e[1m%s:%d\e[0m with error: %s (%d)\n", file, line, cusparseGetErrorString(status), status);
+        exit(EXIT_FAILURE);
+    }
+}
+
+#define CHECK_CUDA(err) (check_cuda_error(err, __FILE__, __LINE__))
+#define CHECK_CUSPARSE(err) (check_cusparse_error(err, __FILE__, __LINE__))
 
 #endif
